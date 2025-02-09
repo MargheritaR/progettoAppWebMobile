d86efff4aaf184957b833c48873f2940419b05a7
Danil-00 <148452674+Danil-00@users.noreply.github.com>
2025-02-09 19:30:23 +0100

    modifica dell'index con aggiunta del cors

--- Back-End/index.js
+++ Back-End/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const rotteUtenti = require('./rotte/rotteUtenti');
 const rottePost = require('./rotte/rottePost');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 
 app.use(bodyParser.json());
 
